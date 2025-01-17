3f6fc6dc2e258a3c7ce28a2a10688dfee490f29c
Мария Тарлавина <tarlavina50@mail.ru>
2025-01-17 21:37:40 +0700

    +

--- index.php
+++ index.php
@@ -4,16 +4,24 @@ try {
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connected successfully";
 
-    $sql = "CREATE DATABASE if not exists Users";
-    $conn->exec($sql);
-    echo "Database created successfully";
+    // $sql = "CREATE DATABASE if not exists Users";
+    // $conn->exec($sql);
+    // echo "Database created successfully";
 
-    $sql = "USE Users;
-    CREATE TABLE if not exists Users (
-        user_id int primary key auto_increment,
-        name varchar(30) ,
-        age int
-    )";
+    $sql = "USE Users; INSERT INTO users (name, age) VALUES 
+	('John Doe', 3),
+	('Jane Doe', 4),
+	('John Doe', 5),
+	('Jane Doe', 6),
+	('John Doe', 7),
+	('Jane Doe', 8),
+	('John Doe', 9),
+	('Jane Doe', 10),
+	('John Doe', 11),
+	('Jane Doe', 12),
+	('John Doe', 13),
+	('Jane Doe', 14)
+	";
     $conn->exec($sql);
     echo "Table created successfully";
     $conn = null;
